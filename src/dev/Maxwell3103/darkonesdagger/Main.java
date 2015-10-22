75169911dbce724b240b3680b70350d5daf2808f
mdoesmc <mathematickid@gmail.com>
2015-10-22 18:28:43 -0500

    Project Setup

---
+++ src/dev/Maxwell3103/darkonesdagger/Main.java
@@ -0,0 +1,19 @@
+package dev.Maxwell3103.darkonesdagger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		return true;
+	}
+}
